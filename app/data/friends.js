8ed7bc13e18aa8cc87e2239ff6099c48a165637c
Ana Todorovic <todorovica2137@gmail.com>
2019-10-04 13:10:36 -0700

    survey

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+var friendsArr = [
+{
+    name:"Maria",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 3]
+},
+{
+    name:"John",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[1, 5, 2, 3, 4, 1, 1, 2, 4, 5]
+},
+{
+    name:"Erica",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[5, 4, 2, 2, 4, 3, 1, 1, 3, 2]
+},
+];
